94b31091908614bcd5a870da01a2acf2a90241fe
Sarah Tuitoek <sjtoek@gmail.com>
2023-02-21 23:37:51 -0800

    Edit file

--- 0x02-functons_nested_loops/0-putchar.c
+++ 0x02-functons_nested_loops/0-putchar.c
@@ -1,33 +1,23 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
- * Description: _putchar  prints _putchar
+ * Description: putchar prints characters
  *
- * Return: 1 if putchar is printed
+ * Return: 1 if success else 0
  */
-
 int main(void)
 {
-	/**
-	 * Declare and initialize array of char
-	 * Declare and initialize int i to 0
-	 * While loop
-	 * while _ putchar at index i is not equal to '\0'
-	 * Print char at index i
-	 */
-
-	char ch[] = "_putchar";
+	char c[] = "_putchar";
 	int i;
 
 	i = 0;
-	while (ch[i] != '\0')
+	while (c[i] != '\0')
 	{
-		_putchar(ch[i]);
+		_putchar(c[i]);
 		i++;
 	}
 	_putchar('\n');
 	return (0);
-
 }
-
