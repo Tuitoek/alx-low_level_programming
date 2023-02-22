019934b3eaa0594d69cdff73ab9f8e18f287a2ae
Sarah Tuitoek <sjtoek@gmail.com>
2023-02-21 20:56:12 -0800

    Print putchar

---
+++ 0x02-functons_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: printf print _putchar
+ *
+ * Return: 1 if putchar is printed
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
