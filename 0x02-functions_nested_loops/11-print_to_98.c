db8d63af8e5e758af27d55f33dd795461793fd18
Sarah Tuitoek <sjtoek@gmail.com>
2023-02-23 02:04:27 -0800

    Print till 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,20 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (x = n; x <= 98; x++)
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+		printf("%d\n", n);
+	} else if (n > 98)
+	{
+		for (x = n; x > 98; x--)
 		{
 			printf("%d, ", x);
 		}
+		printf("%d\n", n);
 	} else
 	{
-		for (x = n; x >= 98; x--)
-		{
-			printf("%d, ", x);
-		}
+		printf("%d\n", n);
 	}
-	printf("\n");
 }
