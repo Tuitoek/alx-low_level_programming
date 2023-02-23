41ae4b3c0be8254f9d9e0a6451f7951b76b39923
Sarah Tuitoek <sjtoek@gmail.com>
2023-02-22 23:24:12 -0800

    Addd two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds intergers
+ *
+ * Description: adds two integers
+ * @s: first input
+ * @u: second input
+ *
+ * Return: added interger
+ */
+int add(int s, int u)
+{
+	int t = s + u;
+
+	return (t);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.c"
+
+/**
+ * times_table - Entry point
+ *
+ * Description: prints 9 times table
+ */
+
+void times_table(void)
+{}
