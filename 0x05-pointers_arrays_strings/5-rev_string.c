f02f386346e5d451f5c7a22f945554692f7d4a56
Sarah Tuitoek <sjtoek@gmail.com>
2023-03-01 02:44:54 -0800

    Print in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,15 +12,15 @@ void rev_string(char *s)
 	char t;
 
 	/* get length of array s*/
+	len = 0;
 	while (*(s + len) != '\0')
 		len++;
 
 	j = len - 1;
-	for (i = 0; j >= 0 && i < j; i++, j--)
+	for (i = 0; j >= 0 && i < j; i--,j++)
 	{
-		t = s[j];
-		s[j] = s[i];
-		s[i] = t;
+		t = s[i];
+		s[i] = s[j];
+		s[j] = t;
 	}
-
 }
